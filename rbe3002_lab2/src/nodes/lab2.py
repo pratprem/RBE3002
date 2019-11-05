1105753acf6e554e4533d28f17f3d49a466ae611
Arjun Gandhi <arjungandhi06@gmail.com>
2019-11-05 12:30:40 -0500

    added code done during thursday lab session

---
+++ rbe3002_lab2/src/nodes/lab2.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python2
+
+import rospy
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Twist
+from tf.transformations import euler_from_quaternion
+
+class Lab2:
+
+    def __init__(self):
+        """
+        Class constructor
+        """
+        ### Initialize node, name it 'lab2'
+        rospy.init_node('lab2')
+        ### Tell ROS that this node publishes Twist messages on the '/cmd_vel' topic
+        self.cmd_vel = rospy.Publisher('/cmd_vel', Twist)
+        #subscribing to odom and pose nodes
+        rospy.Subscriber('/odom', Odometry , self.update_odometry)
+        rospy.Subscriber('/move_base_simple/goal', PoseStamped , self.update_odometry)
+        #initiallizing variables needed
+        self.px=0.0
+        self.py=0.0
+        self.pth=0.0
+
+
+
+    def send_speed(self, linear_speed, angular_speed):
+        """
+        Sends the speeds to the motors.
+        :param linear_speed  [float] [m/s]   The forward linear speed.
+        :param angular_speed [float] [rad/s] The angular speed for rotating around the body center.
+        """
+        # Create twist message
+        msg_cmd_vel = Twist()
+        # Linear velocity
+        msg_cmd_vel.linear.x = linear_speed
+        msg_cmd_vel.linear.y = 0.0
+        msg_cmd_vel.linear.z = 0.0
+        # Angular velocity
+        msg_cmd_vel.angular.x = 0.0
+        msg_cmd_vel.angular.y = 0.0
+        msg_cmd_vel.angular.z = angular_speed
+        # Send command
+        self.cmd_vel.publish(msg_cmd_vel)
+
+
+
+    def drive(self, distance, linear_speed):
+        """
+        Drives the robot in a straight line.
+        :param distance     [float] [m]   The distance to cover.
+        :param linear_speed [float] [m/s] The forward linear speed.
+        """
+        #save initial pose
+        init_pose=[self.px,self.py,self.pth]
+        while
+
+
+
+    def rotate(self, angle, aspeed):
+        """
+        Rotates the robot around the body center by the given angle.
+        :param angle         [float] [rad]   The distance to cover.
+        :param angular_speed [float] [rad/s] The angular speed.
+        """
+        ### REQUIRED CREDIT
+        pass # delete this when you implement your code
+
+
+
+    def go_to(self, msg):
+        """
+        Calls rotate(), drive(), and rotate() to attain a given pose.
+        This method is a callback bound to a Subscriber.
+        :param msg [PoseStamped] The target pose.
+        """
+        print('hallo')
+
+
+
+    def update_odometry(self, msg):
+        """
+        Updates the current pose of the robot.
+        This method is a callback bound to a Subscriber.
+        :param msg [Odometry] The current odometry information.
+        """
+        self.px=msg.pose.pose.position.x
+        self.py=msg.pose.pose.position.y
+        quat_orig = msg.pose.pose.orientation
+        quat_list = [quat_orig.x, quat_orig.y, quat_orig.z, quat_orig.w]
+        (roll , pitch , yaw) = euler_from_quaternion (quat_list)
+        self.pth = yaw
+
+
+
+    def arc_to(self, position):
+        """
+        Drives to a given position in an arc.
+        :param msg [PoseStamped] The target pose.
+        """
+        ### EXTRA CREDIT
+        # TODO
+        pass # delete this when you implement your code
+
+
+
+    def smooth_drive(self, distance, linear_speed):
+        """
+        Drives the robot in a straight line by changing the actual speed smoothly.
+        :param distance     [float] [m]   The distance to cover.
+        :param linear_speed [float] [m/s] The maximum forward linear speed.
+        """
+        ### EXTRA CREDIT
+        # TODO
+        pass # delete this when you implement your code
+
+
+
+    def run(self):
+        rospy.spin()
+
+if __name__ == '__main__':
+    Lab2().run()
