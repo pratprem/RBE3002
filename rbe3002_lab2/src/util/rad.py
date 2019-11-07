b79601793b85f0808869f0d60cd885e58e93ed6d
Arjun Gandhi <arjungandhi06@gmail.com>
2019-11-07 17:53:13 -0500

    tested the radian class and moved to the util folder

--- rbe3002_lab2/src/util/rad.py
+++ rbe3002_lab2/src/util/rad.py
@@ -1,29 +1,32 @@
 import math
 
+
 class Radian:
-    def __init__(self,angle,degrees=False):
-        if type(angle) not float
-            raise ValueError('expected type float recieved (!r)'.format(type(angle))
+    def __init__(self, angle, degrees=False):
+        if type(angle) is int:
+            angle=float(angle)
+        elif type(angle) is not float:
+            raise ValueError('expected type float recieved (!r)'.format(type(angle)))
         if degrees:
             angle=_degree_to_radians(a)
-        self.value=self._norm(angle) 
-    
-    def __add__(self,other):
+        self.value=self._norm(angle)
+
+    def __add__(self, other):
         return Radian(self.value+other.value)
 
-    def __sub__(self,other): 
+    def __sub__(self, other):
         return Radian(self.value-other.value)
 
-    def __mul__(self,other):
+    def __mul__(self, other):
         return Radian(self.value*other.value)
 
-    def __truediv__(self,other):
+    def __truediv__(self, other):
         return Radian(self.value/other.value)
 
     def __str__(self):
         return str(self.value)+" radians"
 
-    def __cmp__(self,other):
+    def __cmp__(self, other):
         return int(self.value-other.value)
 
     def _degree_to_radians(a):
@@ -31,7 +34,7 @@ class Radian:
         takes float a and coverts from degrees to radians
         :param a    [float] [radians] angle to conver
         """
-        return 2*math.pi*a/360.0 
+        return 2*math.pi*a/360.0
 
     def norm(*args):
         """
@@ -40,31 +43,33 @@ class Radian:
         :param  args        [float] [radians] an list of angles to wrap
         :return angles      [float] [radians] list of radian object
         """
-        #takes all angles and creates Radian Objects
-        r= [Radian(a) for a in args]
+        # takes all angles and creates Radian Objects
+        r=[Radian(a) for a in args]
 
-        #if len of result is 1 unpack tuple else dont touch
-        if len(result)==1:
+        # if len of result is 1 unpack tuple else dont touch
+        if len(result) == 1:
             return result[0]
         else:
             return result
         return result
 
 
-    def _norm(self,*args):
+    def _norm(self, *args):
         """
         internal functions to wrap all angles into -pi to pi space
         :param  args        [float] [radians] an list of angles to wrap
         :return angles      [float] [radians] list of angles returned -pi to pi
         """
-        #convert angle to 0 to 2pi space
-        r= [math.fmod(math.fmod(a,2*math.pi) +2*math.pi,2*math.pi) for a in args]
-        #convert from wrap angle form pi to 2pi to -pi to zero
-        result = tuple([a-2*math.pi if a>math.pi else a for a in r]) 
+        # convert angle to 0 to 2pi space
+        r=[math.fmod(math.fmod(a, 2*math.pi) + 2*math.pi, 2*math.pi)
+                     for a in args]
+        # convert from wrap angle form pi to 2pi to -pi to zero
+        result=tuple([a-2*math.pi if a >= math.pi else a for a in r])
 
-        #if len of result is 1 unpack tuple else dont touch
-        if len(result)==1:
+        # if len of result is 1 unpack tuple else dont touch
+        if len(result) == 1:
             return result[0]
         else:
             return result
         return result
+
